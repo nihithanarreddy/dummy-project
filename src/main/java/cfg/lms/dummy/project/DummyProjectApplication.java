109ca6ce5df41a2c731355d3ebe9da0ab60e3e3f
nihithanarreddy <nihithanarreddy@gmail.com>
2025-07-17 11:36:26 +0530

    intial project setup

---
+++ src/main/java/cfg/lms/dummy/project/DummyProjectApplication.java
@@ -0,0 +1,13 @@
+package cfg.lms.dummy.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyProjectApplication.class, args);
+	}
+
+}
